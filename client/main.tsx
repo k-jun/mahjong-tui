7ac5feab41ffd8be310aa057e8314ee790c6f634
k-jun <keijun091221@gmail.com>
2025-06-21 02:23:54 +0900

    feat: v0.2.1

--- client/main.tsx
+++ client/main.tsx
@@ -203,7 +203,7 @@ const main = async (endpoint: string) => {
 
 await new Command()
   .name("mahjong-tui")
-  .version("1.0.0")
+  .version("0.2.1")
   .description("Mahjong TUI")
   .helpOption("--help", "Print help info.")
   .option("-h, --host <host>", "Host", "mahjong-tui.k-jun.net")
